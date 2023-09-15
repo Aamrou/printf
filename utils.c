ad7451d1e8c4841a3b1280335d1da3e43f13cd01
erasekhilyass <136187314+erasekhilyass@users.noreply.github.com>
2023-09-15 17:17:39 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
- * is_printable - Evaluates if a char is printable
+ * is_printable - thee f that Evaluates if a char is printable.
  * @c: Char to be evaluated.
- *
- * Return: 1 if c is printable, 0 otherwise
+ * Return: 1 if c is printable, 0 otherwise.
  */
 int is_printable(char c)
 {
@@ -13,13 +11,12 @@ int is_printable(char c)
 
 	return (0);
 }
-
 /**
  * append_hexa_code - Append ascci in hexadecimal code to buffer
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
- * Return: Always 3
+ * Return: it should Always return a 3.
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
@@ -27,16 +24,12 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
-
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
-
 	buffer[i++] = map_to[ascii_code / 16];
 	buffer[i] = map_to[ascii_code % 16];
-
 	return (3);
 }
-
 /**
  * is_digit - Verifies if a char is a digit
  * @c: Char to be evaluated
