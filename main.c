f517308ec37f25c2fdd3a459184aefe61b15607c
erasekhilyass <136187314+erasekhilyass@users.noreply.github.com>
2023-09-15 17:14:30 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,9 @@
 #include <limits.h>
 #include <stdio.h>
 #include "main.h"
-
 /**
- * main - Entry point
- *
- * Return: Always 0
+ * main - Entry point.
+ * Return: it should Always return a 0.
  */
 int main(void)
 {
